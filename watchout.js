45b9c978bc370e9a14518a46acd4210e159dffd4
Hack Reactor Students <rgoldfinger@gmail.com>
2014-06-13 18:48:06 -0700

    Implement moving enemies and responsive screen-sizing

--- watchout.js
+++ watchout.js
@@ -1,8 +1,8 @@
 // start slingin' some d3 here.
 var options = {
-  height: 600,
-  width: 900,
-  enemies: 13
+  height: $(window).height(),
+  width: $(window).width(),
+  enemies: 43
 };
 
 //enemies
@@ -37,11 +37,55 @@ var board = d3.select('body')
   .attr('height', options.height)
   .attr('width', options.width);
 
-board.selectAll('circle')
-  .data(generateEnemies(options.enemies), function(d) { return d.i; })
-  .enter()
-  .append('svg:circle')
-  .attr('cx', function(d) { return d.x; })
-  .attr('cy', function(d) { return d.y; })
-  .attr('r', function(d) { return d.r; })
-  .attr('fill', function(d) { return d.color; });
+
+var moveEnemies = function() {
+
+  //join our data with the enemies on the board
+  var enemies = board.selectAll('circle')
+    .data(generateEnemies(options.enemies), function(d) { return d.i; });
+
+
+
+  //update existing enemies with transitions for relocation
+  enemies.transition().duration(1000)
+    .attr('cx', function(d) { return d.x; })
+    .attr('cy', function(d) { return d.y; });
+
+
+  //enter = create new enemies
+
+  enemies.enter().append('svg:circle')
+    .attr('r', function(d) { return d.r; })
+    .attr('fill', function(d) { return d.color; })
+    .attr('cx', function(d) { return d.x; })
+    .attr('cy', function(d) { return d.y; });
+
+  //update and enter with current positions
+  enemies
+};
+
+setInterval(moveEnemies, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
