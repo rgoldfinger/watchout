aee281fe9964c0d91505935800a7bbb396c3f64d
Hack Reactor Students <rgoldfinger@gmail.com>
2014-06-13 17:40:41 -0700

    Create game board, and implement enemy generation and display.

--- watchout.js
+++ watchout.js
@@ -1,2 +1,47 @@
 // start slingin' some d3 here.
+var options = {
+  height: 600,
+  width: 900,
+  enemies: 13
+};
 
+//enemies
+//id, location(x,y), r, color
+var Enemy = function (i) {
+  this.i = i;
+  this.x = Math.floor(Math.random() * options.width);
+  this.y = Math.floor(Math.random() * options.height);
+  this.r = Math.floor(Math.random() * 15) + 15;
+  this.color = this.makeColor();
+
+};
+
+Enemy.prototype.makeColor = function() {
+  var randC = function() {
+    return Math.floor(Math.random() * 256);
+  };
+  return "rgb(" + randC() + "," + randC() + "," + randC() + ")";
+};
+
+var generateEnemies = function(numberOfEnemies) {
+  var results = [];
+  for (var i = 0; i < numberOfEnemies; i++) {
+    results.push(new Enemy(i));
+  }
+  return results;
+};
+
+
+var board = d3.select('body')
+  .append('svg:svg')
+  .attr('height', options.height)
+  .attr('width', options.width);
+
+board.selectAll('circle')
+  .data(generateEnemies(options.enemies), function(d) { return d.i; })
+  .enter()
+  .append('svg:circle')
+  .attr('cx', function(d) { return d.x; })
+  .attr('cy', function(d) { return d.y; })
+  .attr('r', function(d) { return d.r; })
+  .attr('fill', function(d) { return d.color; });
