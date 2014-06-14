f4e021127f038940eaf1ee3cde1cfaa259cc6008
Hack Reactor Students <rgoldfinger@gmail.com>
2014-06-13 19:40:44 -0700

    Implement player polygon

--- watchout.js
+++ watchout.js
@@ -20,7 +20,7 @@ Enemy.prototype.makeColor = function() {
   var randC = function() {
     return Math.floor(Math.random() * 256);
   };
-  return "rgb(" + randC() + "," + randC() + "," + randC() + ")";
+  return 'rgb(' + randC() + ',' + randC() + ',' + randC() + ')';
 };
 
 var generateEnemies = function(numberOfEnemies) {
@@ -31,11 +31,30 @@ var generateEnemies = function(numberOfEnemies) {
   return results;
 };
 
+// var player = {
+
+//   stroke: '#000000',
+//   'stroke-width': 2,
+//   points: '18,0 6,16 -13,10 -13,-10 6,-16 18,0',
+//   fill: '#7f7f7f'
+// };
+
+
+//------------ d3 display -----------------
+//-----------------------------------------
 
 var board = d3.select('body')
   .append('svg:svg')
   .attr('height', options.height)
   .attr('width', options.width);
+
+board.append('polygon')
+  .attr('stroke', '#000000')
+  .attr('stroke-width', 2)
+  .attr('points', '18,0 6,16 -13,10 -13,-10 6,-16 18,0')
+  .attr('fill', '#7f7f7f')
+  .attr('transform', 'translate( '+ options.width / 2 + ',' + options.height / 2 + ')');
+
 
 
 var moveEnemies = function() {
@@ -48,6 +67,7 @@ var moveEnemies = function() {
 
   //update existing enemies with transitions for relocation
   enemies.transition().duration(1000)
+    // .ease('elastic')
     .attr('cx', function(d) { return d.x; })
     .attr('cy', function(d) { return d.y; });
 
@@ -61,7 +81,6 @@ var moveEnemies = function() {
     .attr('cy', function(d) { return d.y; });
 
   //update and enter with current positions
-  enemies
 };
 
 setInterval(moveEnemies, 1000);
